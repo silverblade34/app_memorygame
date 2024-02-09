4bb5b14504fc85697c8990136f6d3ca9ecee416e
Michell Pacheco Tacay <mpacheco.tacay@gmail.com>
2024-02-09 10:28:20 -0500

    se esta ordenando aleatoriamente las imagenes al inicializar el juego

--- lib/app/controllers/home_controller.dart
+++ lib/app/controllers/home_controller.dart
@@ -3,9 +3,28 @@ import 'package:get/get.dart';
 class HomeController extends GetxController {
   final String hiddenCardpath = 'assets/memorygame/hidden.png';
   List<String>? gameImg;
-  final int cardCount = 8;
 
-  void initGame(){
+  final List<String> cards_list = [
+    'assets/memorygame/img1.jpeg',
+    'assets/memorygame/img2.jpeg',
+    'assets/memorygame/img3.jpeg',
+    'assets/memorygame/img4.jpeg',
+    'assets/memorygame/img5.jpeg',
+    'assets/memorygame/img6.jpeg',
+    'assets/memorygame/img1.jpeg',
+    'assets/memorygame/img2.jpeg',
+    'assets/memorygame/img3.jpeg',
+    'assets/memorygame/img4.jpeg',
+    'assets/memorygame/img5.jpeg',
+    'assets/memorygame/img6.jpeg'
+  ];
+
+  List<Map<int, String>> matchCheck = [];
+
+  final int cardCount = 12;
+
+  void initGame() {
+    cards_list.shuffle(); // Barajea los elementos de la lista
     gameImg = List.generate(cardCount, (index) => hiddenCardpath);
   }
 }
--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -12,6 +12,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   HomeController _game = HomeController();
 
+  int tries = 0;
+  int score = 0;
+
   @override
   void initState() {
     super.initState();
@@ -21,61 +24,96 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Color(0xFFe55870),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Center(
-            child: Text(
-              "Memory Game",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 40,
             ),
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [scoreBoard("Intentos", "0"), scoreBoard("Puntos", "0")],
-          ),
-          SizedBox(
-            height: screenWidth,
-            width: screenWidth,
-            child: GridView.builder(
-              itemCount: _game.gameImg!.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
+            const Center(
+              child: Text(
+                "Juego de memoria",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold),
               ),
-              padding: EdgeInsets.all(16),
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(66, 255, 252, 70),
-                      borderRadius: BorderRadius.circular(8),
-                      image: DecorationImage(
-                        image: AssetImage(
-                          _game.gameImg![index],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                scoreBoard("Intentos", "$tries"),
+                scoreBoard("Puntos", "$score")
+              ],
+            ),
+            SizedBox(
+              height: screenHeight - 300,
+              width: screenWidth,
+              child: GridView.builder(
+                itemCount: _game.gameImg!.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                ),
+                padding: const EdgeInsets.all(16),
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      print(_game.cards_list[index]);
+                      setState(() {
+                        tries++;
+                        _game.gameImg![index] = _game.cards_list[index];
+                        _game.matchCheck.add({index: _game.cards_list[index]});
+                      });
+                      if (_game.matchCheck.length == 2) {
+                        if (_game.matchCheck[0].values.first ==
+                            _game.matchCheck[1].values.first) {
+                          print("TRUE");
+                          score += 100;
+                          _game.matchCheck.clear();
+                        } else {
+                          print("FALSE");
+                          Future.delayed(const Duration(milliseconds: 600), () {
+                            setState(() {
+                              _game.gameImg![_game.matchCheck[0].keys.first] =
+                                  _game.hiddenCardpath;
+                              _game.gameImg![_game.matchCheck[1].keys.first] =
+                                  _game.hiddenCardpath;
+                              _game.matchCheck.clear();
+                            });
+                          });
+                        }
+                      }
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(66, 255, 252, 70),
+                        borderRadius: BorderRadius.circular(8),
+                        image: DecorationImage(
+                          image: AssetImage(
+                            _game.gameImg![index],
+                          ),
+                          fit: BoxFit.cover,
                         ),
-                        fit: BoxFit.cover,
                       ),
                     ),
-                  ),
-                );
-              },
-            ),
-          )
-        ],
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
